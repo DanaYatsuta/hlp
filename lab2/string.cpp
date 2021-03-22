02b49d387f6c9de427c7a9591d0e8360948d4d23
Dana Yatsuta <danayatsuta@gmail.com>
2021-03-22 09:41:05 +0300

    Defined copy constructor for string

--- lab2/string.cpp
+++ lab2/string.cpp
@@ -11,3 +11,10 @@ String::String(char* c_string)
     string_begin = new char[length];
     strcpy(string_begin, c_string);
 }
+
+String::String(const String& other)
+{
+    length = other.length;
+    string_begin = new char[length];
+    strcpy(string_begin, other.string_begin);
+}
